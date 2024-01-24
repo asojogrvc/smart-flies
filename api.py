fa81c4992394b3e0c253de5595976fd494736ae3
asojogrvc <asojo@us.es>
2024-01-24 11:24:21 +0100

    Added ability to upload kml files for bases and towers to the server

--- api.py
+++ api.py
@@ -6,63 +6,155 @@ import os
 # Projects internal modules imports
 from modules import bases as BA, towers as TW, uav as UAVS, solver as SO, weather as WT, coordinates as CO, yaml as iYAML
 
+# --------------------------------------------------------------------
+#
+#    Derive the Flask class to allow new parameters and methods
+#
+# --------------------------------------------------------------------
+
 
 class Server(Flask):
     """
     This is a derived class from Flask. It contains all the extra data that needs to be accessed for the
     I/O to happen interactively.
     """
-    __status = "Inactive"
+    __status = "inactive"    # By default, the API server is inactive.
+    __bases_Input = "file"   # How was data received
+    __towers_Input  = "file" # How was data received
 
-    def set_Status(self, statusI):
-        self.__status = statusI
-        return None
+    def set_Status(self, statusI: str):
+
+        allowed_states = ["inactive", "occupied"]
+
+        if statusI in allowed_states:
+            self.__status = statusI
+            return None
+        
+        else:
+            raise Exception("Tried to change the server status to an invalid one.")
 
     def get_Status(self) -> str:
         return self.__status
+    
+    def set_Input_type(self, new_type: str, which:str):
+
+        valid_types = ["file", "json"]
+
+        if new_type in valid_types:
+
+            match which:
+                case "Bases":
+                    self.__bases_Input = new_type
+                case "Towers":
+                    self.__towers_Input = new_type
+                case _:
+                    raise Exception("Not a valid input parameter")
+                
+        else:
+            raise Exception("New type is not valid")
+        
+        return None
+    
+    def get_Input_type(self, which:str) -> str:
+
+        match which:
+            case "Bases":
+                    return self.__bases_Input
+            case "Towers":
+                    return self.__towers_Input
+            case _:
+                raise Exception("Not a valid input parameter")
 
 app = Server(__name__)
 
-# ------------------------ Define the api routes ---------------------
+# --------------------------------------------------------------------
+#
+#                        Define the API routes
+#
+# --------------------------------------------------------------------
 
-@app.route('/input_bases', methods=['POST'])
-def receive_bases():
+
+# --------------------- Data input Routes ----------------------------
+# https://sentry.io/answers/flask-getting-post-data/
+# https://flask.palletsprojects.com/en/2.3.x/patterns/fileuploads/
+
+@app.route('/input_bases', methods = ['POST'])
+def receive_bases() -> dict | None:
     """
-    https://sentry.io/answers/flask-getting-post-data/
+    Receive the needed data to define bases. It can be done as an "application/json" or
+    as an .kml file.
     """
 
-    content_type = request.headers.get('Content-Type')
+    # Check if the request contains a file.
 
-    if (content_type == 'application/json'):
-        return {"output":"Content type is not supported."}
-    else:
-        return {"output":"Content type is not supported."}
+    # If not, consider it a data stream for some content type
+    if 'file' not in request.files:
+
+        content_type = request.headers.get('Content-Type')
+
+        if (content_type == 'application/json'):
+
+            # request.get_json()
+
+            app.set_Input_type("json", "Bases")
+            return {"output":"JSON Received"}
+        else:
+            return {"output":"No file in the request and the stream is not a json"}
     
-@app.route('/input_towers', methods = ['GET', 'POST'])
-def receive_towers():
-    """
-    https://flask.palletsprojects.com/en/2.3.x/patterns/fileuploads/
-    """
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        
+    # If it does, treat the data as a file.
+    else:
+
         file = request.files['file']
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
         if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('download_file', name=filename))
+            return {"output": "No file or json in request"}
+        if file:
 
+            file.save("./server/dynamic/bases.kml")
 
+            app.set_Input_type("file", "Bases")
+            return {"output": "KML Received"}
 
-@app.route('/favicon.ico')
+@app.route('/input_towers', methods = ['POST'])
+def receive_towers() -> dict | None:
+    """
+    Receive the needed data to define the towers. It can be done as an "application/json" or
+    as an .kml file.
+    """
+
+    # Check if the request contains a file.
+
+    # If not, consider it a data stream for some content type
+    if 'file' not in request.files:
+
+        content_type = request.headers.get('Content-Type')
+
+        if (content_type == 'application/json'):
+            
+            # request.get_json()
+
+            app.set_Input_type("json", "Towers")
+            return {"output":"JSON Received"}
+        else:
+            return {"output":"No file in the request and the stream is not a json"}
+    
+    # If it does, treat the data as a file.
+    else:
+
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            return {"output": "No file or json in request"}
+        if file:
+
+            file.save("./server/dynamic/towers.kml")
+
+            app.set_Input_type("file", "Towers")
+            return {"output": "KML Received"}
+
+@app.route('/favicon.ico', methods = ["GET"])
 def favicon():
     """
     Webpage icon for web browsers.
@@ -70,14 +162,14 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'server','static'),
                           'favicon.ico',mimetype='image/vnd.microsoft.icon')
 
-@app.route("/")
+@app.route("/", methods = ["GET"])
 def main_page():
     """
     Main Page. Put some logos, copyright and stuff here.
     """
     return "</p>If you can see this, the server is online</p>"
 
-@app.route("/status")
+@app.route("/status", methods = ["GET"])
 def status():
     """
     Outputs the current status of the server
@@ -85,14 +177,14 @@ def status():
 
     return {"status": app.get_Status()}
 
-@app.route("/output")
+@app.route("/output", methods = ["GET"])
 def yaml_output():
     """
     Outputs the file last file mission if it exists
     """
-    return send_from_directory("./server/", 'mission.yaml')
+    return send_from_directory("./server/dynamic/", 'mission.yaml')
 
-@app.route("/mission_request")
+@app.route("/mission_request", methods = ["GET"])
 def planner():
 
     # If the server is already doing some computations,
@@ -101,14 +193,17 @@ def planner():
     if "Occupied" == app.get_Status():
         return {"status": "Occupied"}
     
-    app.set_Status("Occupied")
+    app.set_Status("occupied")
     
     # --- Placeholder data -------------------------
+
     bases = BA.Bases()
-    bases.load_File("./files/bases.kml", True)
+    if "file" == app.get_Input_type("Bases"): bases.load_File("./server/dynamic/bases.kml", True)
+    else: bases.load_File("./files/bases.kml", True) # TBI
 
     towers = TW.Towers()
-    towers.load_File("./files/towers.kml", True)
+    if "file" == app.get_Input_type("Towers"): towers.load_File("./server/dynamic/towers.kml", True)
+    else: towers.load_File("./files/towers.kml", True) # TBI
 
     uavs = UAVS.UAV_Team()
     uavs.load_File("./files/UAV_team.xml")
@@ -122,8 +217,8 @@ def planner():
     problem.solve("abstract")
 
     uavs.compute_Team_Waypoints("PaV", towers, bases)
-    iYAML.save_Mission("./server/mission.yaml", uavs, base1.get_UTM_Zone())
+    iYAML.save_Mission("./server/dynamic/mission.yaml", uavs, base1.get_UTM_Zone())
         
-    app.set_Status("Inactive")
+    app.set_Status("inactive")
         
-    return {"result" : "The problem has been solved successfully. Updated file from /yaml_output"}
+    return {"output" : "The problem has been solved successfully. Updated file from /output"}
