5697e13f4cf6a34e3942abce06a8eaec4526e039
asojogrvc <asojo@us.es>
2024-04-09 12:09:10 +0200

    Solver implementation 1

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
 import numpy as np, matplotlib.pyplot as plt
 
-from modules import bases as BA, uavs as UAVS
+from modules import bases as BA, tasks as TS, uavs as UAVS
 
 bases = BA.Bases()
 bases.add_Base(
     "B0",
-    np.array([0,0,0])
+    np.array([0, 0, 0])
 )
 bases.add_Base(
     "B1",
-    np.array([0,3,3])
+    np.array([0, 3, 3])
 )
 
-bases.print()
+towers = TS.Towers()
+towers.add_Tower(
+    "T1",
+    np.array([1,1,1])
+)
+towers.add_Tower(
+    "T2",
+    np.array([1,2,1])
+)
+towers.add_Tower(
+    "T3",
+    np.array([2,2,1])
+)
+
+towers.add_Power_Lines([("T1", "T2"), ("T2", "T3")])
+
+tasks = TS.Tasks()
+tasks.add_Task("Task1", tower = "T1")
+tasks.add_Task("Task2", pair_of_towers = ("T2", "T3"))
+tasks.print()
 
 fig = plt.figure()
 axes = fig.add_subplot(111)
 
 bases.plot(axes)
+towers.plot(axes)
+
 plt.show()
--- modules/tasks.py
+++ modules/tasks.py
@@ -1,193 +1,64 @@
-# Tower and Towers class
+# Tasks and Towers class
 
-import numpy as np
-import re
-from pykml import parser
-import copy
-import modules.coordinates as CO
-import itertools
-import networkx as nx
-import matplotlib.pyplot as plt
-
+import numpy as np, networkx as nx, matplotlib.pyplot as plt
 
 
 class Towers():
     """
-    Towers are represented as a networkx graph.
-        - Each node represents a single tower and contains its name, UTM coordinates (3D)
-        - Each edge represents wire connections between towers
-    It is assumed that all towers are within the same UTM Zone so we only need the local coordinates
+    A graph representation of the towers.
     """
 
     def __init__(self):
         self.__graph = nx.empty_graph(0)
-        self.__UTM_Zone = (0, "A")
 
     # ---------------------------------- Parameter settings -------------------------------------
     
-    def add_Tower(self, name: str, utm_Coordinates: np.ndarray, connections: list):
+    def add_Tower(self, name: str, position: np.ndarray, **kwargs):
         """
         Add tower (or node) to the towers graph based on its coordinates and a list
         of connections
         """
+
+        self.__graph.add_node(name, position = position)
+        if "connections" in kwargs:
+            connections = kwargs["connections"]
+            self.__graph.add_edges_from(connections)
         
-        self.__graph.add_node(name, UTM = utm_Coordinates)
-        self.__graph.add_edges_from(connections)  # As it is, it will allow you to change 
-                                                # interal connections that do not concern 
-                                                # the added tower
-        
-        return None
-        
-    def set_UTM_Zone(self, zone: tuple):
-        """
-        Sets the UTM Zone of the towers. It is assumed that all towers are within the same zone.
-        "zone" is a tuple (Z_number, Z_ letter)
-        """
-        self.__UTM_Zone = zone
-        return None
-
-    def load_File(self, file: str, onlineQ: bool):
-        """
-        Method that updates a tower graph using an offline file. Files need to follow Google Earth "kml"s
-        format containing paths. As paths cannot represent several branches, the will be represented
-        as path with at much one common tower per branch pair.
-        If the boolean "onlineQ" is True, height coordinates will be updated online
-        """
-
-        # Check if the file a .kml
-        if file.split(".")[-1] != "kml":
-            raise Exception("File is not a .kml")
-
-        # Reset first to avoid any data overlap
-        self.reset()
-
-        # Load raw kml file
-        with open(file) as f:
-            doc = parser.parse(f).getroot()
-            f.close()
-
-        # Parse data to extract paths
-        paths = [] # Contains all paths within the file a graph or Towers
-        tower_number = 1
-        for path in doc.Document.Placemark.MultiGeometry.LineString:
-            
-            # temp graph to store each path / branch
-            Tg = nx.empty_graph(0)
-
-            # Parsing each tower latitude-longitude coordinates within a path
-            path_str = np.array(re.split(',| ', path.coordinates.text.strip()))
-            n_t = int(len(path_str)/3)
-            path_latlon = path_str.astype(float).reshape((n_t, 3)) # Numerical conv and reshape to matrix
-            
-            # the kml puts longitude first, lets flip lat and long
-            path_latlon[:, [0,1]] = path_latlon[:, [1, 0]] 
-
-            # Update height from online API
-            if onlineQ:
-                CO.update_Height_Online(path_latlon)
-
-            # Conversion to UTM. All towers and bases are assumed to be within the same zone
-            path_utm, self.__UTM_Zone = CO.latlon2utm(path_latlon)
-
-            # Add each of the towers of the branch in the actual graph 
-            # and connect it to the previously added tower.
-            Tg.add_node(f'T{tower_number}', UTM=path_utm[0,:])
-            tower_number += 1
-            for k in range(1, n_t):
-                Tg.add_node(f'T{tower_number}', UTM = path_utm[k,:])
-                Tg.add_edge(f'T{tower_number-1}', f'T{tower_number}')
-                tower_number += 1
-
-            paths.append(copy.deepcopy(Tg))
-
-        self.__graph = delete_Common_Towers(paths)
         return None
     
-    def load_from_Arrays(self, list_arrays:list, onlineQ: bool):
+    def add_Power_Lines(self, list_of_pairs: list):
         """
-        Method that updates a tower graph using a list of arrays, one for each path. As paths cannot represent several branches, the will be represented
-        as path with at much at least one common tower per branch pair.
-        If the boolean "onlineQ" is True, height coordinates will be updated online
+        Add tower to tower connections given a list of tuples.
         """
 
-        # Reset first to avoid any data overlap
-        self.reset()
-
-        # Parse data to extract paths
-        paths = [] # Contains all paths within the file a graph or Towers
-        tower_number = 1
-        for path_latlon in list_arrays:
-            
-            # temp graph to store each path / branch
-            Tg = nx.empty_graph(0)
-
-            n_t = len(path_latlon)
-
-            # Update height from online API
-            if onlineQ:
-                CO.update_Height_Online(path_latlon)
-
-            # Conversion to UTM. All towers and bases are assumed to be within the same zone
-            path_utm, self.__UTM_Zone = CO.latlon2utm(path_latlon)
-
-            # Add each of the towers of the branch in the actual graph 
-            # and connect it to the previously added tower.
-            Tg.add_node(f'T{tower_number}', UTM=path_utm[0,:])
-            tower_number += 1
-            for k in range(1, n_t):
-                Tg.add_node(f'T{tower_number}', UTM = path_utm[k,:])
-                Tg.add_edge(f'T{tower_number-1}', f'T{tower_number}')
-                tower_number += 1
-
-            paths.append(copy.deepcopy(Tg))
-
-        self.__graph = delete_Common_Towers(paths)
+        try:
+            if tuple == type(list_of_pairs):
+                self.__graph.add_edges_from([list_of_pairs])
+            else:
+                for pair in list_of_pairs:
+                    self.__graph.add_edges_from(list_of_pairs)
+        except:
+            raise Exception("Some connections are not valid")
+        
         return None
-
+    
+    
     def reset(self):
         """
         Reset the towers list to its empty default status
         """
         self.__graph = nx.empty_graph(0)
-        self.__UTM_Zone = (0, "A")
 
         return None
 
     # ---------------------------------- Parameter Output -------------------------------------
-
-    def get_ArrayCoordinates(self) -> np.ndarray:
-        """
-        Get an array that contains all the UTM coordinates of the present towers
-        and its UTM zone as a tuple (Z_Number, Z_Letter)
-        """
-
-        return np.array(list(nx.get_node_attributes(self.__graph,'UTM').values()))
     
-    def get_DictCoordinates(self) -> dict:
+    def get_Positions(self) -> dict:
         """
-        Gets a dict that contains all the UTM coordinates of the present towers
-        and its UTM zone as a tuple (Z_Number, Z_Letter)
+        Gets a dict that contains all the position of the present towers
         """
 
-        return nx.get_node_attributes(self.__graph,'UTM')
-    
-    def get_Towers(self) -> dict:
-        """
-        Returns a dictionary with all present towers as keys and all of their attributes (as a dict) as the values
-        """
-        return self.__graph.nodes(data = True)
-
-    def get_Tower_Coordinates(self, which: str) -> np.ndarray:
-        """
-        Gets the UTM coordinates of an specific tower
-        """
-        return self.__graph.nodes[which]["UTM"]
-    
-    def get_UTM_Zone(self) -> tuple:
-        """
-        Gets the assigned UTM zone as a tuple (Z_Number, Z_Letter)
-        """
-        return self.__UTM_Zone
+        return nx.get_node_attributes(self.__graph,'position')
     
     def get_Graph(self) -> nx.MultiDiGraph:
         """
@@ -195,32 +66,13 @@ class Towers():
         """
         return self.__graph
 
-    def print(self):
-        """
-        Prints the towers data
-        """
-        coords = self.get_DictCoordinates()
-        utm_zone = self.get_UTM_Zone()
-
-        print("----------------------------Towers------------------------------")
-        for tower in coords:
-
-            print("  ", end = "")
-            print(tower+" with UTM: "+str(coords[tower]))
-
-        print("")
-        print("                                                     at "+str(utm_zone[0])+utm_zone[1])
-        print("----------------------------------------------------------------")
-
-        return None
-
     # ---------------------------------- Plotting ------------------------------------------------
 
     def plot(self, axes: plt.Axes):
         """
         Plots the towers using their UTM Coordinates into an existing Matplotlib axes
         """
-        pos3D = self.get_DictCoordinates()
+        pos3D = self.get_Positions()
 
         # Project it into 2D
         pos2D = {}
@@ -228,69 +80,60 @@ class Towers():
             pos2D[key] = pos3D[key][0:2]
 
         nx.draw_networkx(self.__graph, pos = pos2D, ax=axes, with_labels = True)
-
-#-------------------------------------- General Functions -----------------------------------------------
         
-def check_Common_Towers(T1: nx.Graph, T2: nx.Graph, threshold: float) -> list:
+class Tasks():
     """
-    Outputs a list of pair of names of common towers of graph T1 and T2 based on a 2D distance threshold.
-    The first name of each pair is the tower name in T1 and the second in T2     
+    Tasks are either point inspection or lineal inspection. This is a list of then.
     """
 
-    # For some reason, using height too breaks everything. Also, as it needs to wait for the API,
-    # if a file is loaded several times in a rapid succesion, it will also break.
+    def __init__(self):
 
-    common = []
+        self.__list = {} # name, data. data is either a str with the tower name or a tuple for two names
 
-    # Check for every possible pair of towers of each branch if the distance is short enough
-    for pair in list(itertools.product(T1.nodes, T2.nodes)):
-        if (np.linalg.norm(T1.nodes[pair[0]]['UTM'][0:2]-T2.nodes[pair[1]]['UTM'][0:2]) < threshold):
-            # If it is, add the pair to the common towers list
-            common.append((pair[0], pair[1]))
+    def add_Task(self, name: str, **kwargs):
 
-    return common
+        if "tower" in kwargs:
 
-def delete_Common_Towers(paths: list) -> nx.Graph:
-    """
-    Given a list of paths (as Networkx graphs) it removes all repeated towers, connect all paths with common towers
-    and rename towers based on distance to the origin. It outputs the entire grid defined by all paths 
-    as a Networkx Graph
-    """
+            if str == type(kwargs["tower"]):
+                self.__list[name] = kwargs["tower"]
+            else:
+                raise Exception(" 'tower' is not a valid tuple")
+            return None
+        
+        elif "pair_of_towers" in kwargs:
+
+            if tuple == type(kwargs["pair_of_towers"]) and 2 == len(kwargs["pair_of_towers"]):
+                self.__list[name] = kwargs["pair_of_towers"]
+            else:
+                raise Exception(" 'pair_of_towers' is not a valid tuple")
+            
+            return None
+        
+    def print(self):
+
+        print("----------------------------Task List-----------------------------")
+        for task, geometry in self:
+            print(" - Name: ", task, " Inspection of: ", geometry)
+        print("------------------------------------------------------------------")
+
+
+        return None
     
-    # Just check the pair paths that are not redundant
-    indeces_set = list(itertools.combinations(range(len(paths)), 2))
- 
-    for indeces in indeces_set:
-        i = indeces[0]
-        j = indeces[1]
 
-        # Gets the list of common towers (or nodes) of paths[i] and paths[j]
-        C = check_Common_Towers(paths[i], paths[j], 15)
+    # -------------------------------- Iterator definition -------------------------------------
 
-        # If there is at least 1 common tower, put the same number to the tower on both paths
-        if not(len(C) == 0):
-            for pair in C:
-                paths[i] = nx.relabel_nodes(paths[i], {pair[0]:pair[0]+"D"}, copy = False)
-                paths[j] = nx.relabel_nodes(paths[j], {pair[1]:pair[0]+"D"}, copy = False)
-                # We add a "D" of duplicated to avoid overlapping with any other tower
-                
+    def __iter__(self) -> "Tasks":
+        self.__iter_counter = 0
+        self.__iter_max = len(self.__list)
+        return self
 
-    # networkx allow you to compose two graphs. If several nodes are present on two graphs with
-    # the same names, then the composition will combine those nodes into one. This allow
-    # us to delete duplicate towers.
 
-    # Composing every path with renamed towers
-    T = nx.empty_graph(0)
-    for graph in paths:
-         T = nx.compose(graph, T)
+    def __next__(self) -> tuple:
+        # Sets maximum number of iterations
+        if self.__iter_counter < (self.__iter_max):
+            self.__iter_counter += 1
 
-    # Rename Towers based distance to origin.
-    k = 1
-    mapping = {}
-    nodes_sorted = sorted(T.nodes(), key=lambda n: np.linalg.norm(T.nodes[n]['UTM']))
-    for node in nodes_sorted:
-        mapping[node] = f'T{k}'
-        k += 1
-    
-    # Outputs renamed graph
-    return nx.relabel_nodes(T, mapping, copy = True)
+            name = list(self.__list.keys())[self.__iter_counter-1]
+            return name, self.__list[name]
+        else:
+            raise StopIteration
