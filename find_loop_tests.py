af0e1001af0594357234af84c15dc8a1632aea4c
asojogrvc <asojo@us.es>
2024-04-16 08:48:03 +0200

    Deleted file

--- find_loop_tests.py
+++
@@ -1,5 +0,0 @@
-from modules import solver as SO
-
-route = [('tT5', 'B1'), ('B1', 'tS3_D'), ('tT1', 'tT2'), ('tT2', 'tT1'), ('tT3', 'tT4'), ('tT4', 'tT3'), ('tS1_D', 'tT5'), ('tS3_D', 'tS1_D')]
-
-SO.list_Loops(route)
